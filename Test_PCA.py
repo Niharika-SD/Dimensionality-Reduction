d592cfface1b21e9a5737acde8bc64559dd5fc56
ShimonaNiharika <Shimona.Niharika.Dsouza@gmail.com>
2017-01-04 06:34:42 -0500

    Implementation of PCA from direct principles

--- Test_PCA.py
+++ Test_PCA.py
@@ -2,12 +2,6 @@ import numpy as np
 
 np.random.seed(2) # random seed for consistency
 
-# A reader pointed out that Python 2.7 would raise a
-# "ValueError: object of too small depth for desired array".
-# This can be avoided by choosing a smaller random seed, e.g. 1
-# or by completely omitting this line, since I just used the random seed for
-# consistency.
-
 mu_vec1 = np.array([0,0,0])
 cov_mat1 = np.array([[1,0,0],[0,1,0],[0,0,1]])
 class1_sample = np.random.multivariate_normal(mu_vec1, cov_mat1, 20).T
