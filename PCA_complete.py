5742ddcd627bf73717c2150567f6930179000398
ShimonaNiharika <Shimona.Niharika.Dsouza@gmail.com>
2017-01-05 08:19:13 -0500

    kPCA + PCA on dataset

--- PCA_complete.py
+++ PCA_complete.py
@@ -4,6 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d import proj3d
 from pylab import *
 from sklearn.decomposition import PCA as sklearnPCA
+from sklearn.decomposition import KernelPCA as sklearnKPCA
 import scipy.io as sio
 import os
 
@@ -18,4 +19,9 @@ sklearn_transf = sklearn_pca.fit_transform(data_train)
 print(sklearn_pca.explained_variance_ratio_)
 
 red_data_test = sklearn_pca.transform(data_test)
+print(red_data_test.shape)
+
+sklearn_kpca = sklearnKPCA(n_components=10,kernel="rbf")
+sklearn_transf_kpca = sklearn_kpca.fit_transform(data_train)
+red_data_test = sklearn_kpca.transform(data_test)
 print(red_data_test.shape)
